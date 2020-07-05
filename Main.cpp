d890b50f60be3efd7ea40adf6867dfeb83d4405b
SorraKW <sorrawit456879@gmail.com>
2020-07-05 15:41:05 +0700

    Add logout and error multiple input etc. detail is in README

--- Main.cpp
+++ Main.cpp
@@ -5,8 +5,8 @@ class account
 {
 private:
   int money = 0;
-  std::string PV_Username = "Sorrawit";
-  std::string PV_Password = "1234";
+  std::string PV_Username;
+  std::string PV_Password;
 
 public:
   account(std::string user, std::string pass);
@@ -21,8 +21,7 @@ void login();
 
 int main(int argc, char *argv[])
 {
-  std::cout << "BANK SYSTEM BY SORRAWIT\n";
-  std::cout << "Please Login\n";
+  std::cout << "BANK SYSTEM BY SORRAWIT\n\n";
   login();
 
   std::cin.get();
@@ -33,6 +32,7 @@ void login()
 {
   std::string username;
   std::string password;
+  std::cout << "Please Login or Create Your account\n";
   std::cout << "Username : ";
   std::cin >> username;
   std::cout << "Password : ";
@@ -42,6 +42,12 @@ void login()
 
 account::account(std::string user, std::string pass)
 {
+  if(PV_Username.empty() && PV_Password.empty())
+  {
+    PV_Username = user;
+    PV_Password = pass;
+    std::cout << "Account Created!\n";
+  }
   if (user == PV_Username && pass == PV_Password)
   {
     std::cout << "Login Success!\n";
@@ -74,24 +80,25 @@ void account::deleteAcc()
 
 void account::displayLoop()
 {
-  char ch;
+  int ch;
 
   do
   {
-    std::cout << "\t--------MAIN MENU--------\n";
-    std::cout << "\tAccount Statement : \t1\n";
-    std::cout << "\tDeposit : \t\t2\n";
-    std::cout << "\tWithdraw : \t\t3\n";
-    std::cout << "\tDelete Account : \t4\n";
-    std::cout << "\tExit : \t\t\t5\n";
+    std::cout << "\t|--------MAIN MENU--------|\n";
+    std::cout << "\t|Account Statement : \t1 |\n";
+    std::cout << "\t|Deposit : \t\t2 |\n";
+    std::cout << "\t|Withdraw : \t\t3 |\n";
+    std::cout << "\t|Delete Account : \t4 |\n";
+    std::cout << "\t|Exit : \t\t5 |\n";
+    std::cout << "\t|-------------------------|\n";
     std::cout << "Enter menu: ";
     std::cin >> ch;
     switch (ch)
     {
-    case '1':
+    case 1:
       std::cout << "Account Statement : " << getMoney() << '\n';
       break;
-    case '2':
+    case 2:
       int de_money;
       std::cout << "Deposit Money\n";
       std::cout << "Enter the amount of money you want to deposit. : ";
@@ -99,15 +106,20 @@ void account::displayLoop()
       deposit(de_money);
       std::cout << "It's Done!\n";
       break;
-    case '3':
+    case 3:
+      if(getMoney() == 0)
+      {
+        std::cout << "Your current money : 0\nPlease add money to your account first\n";
+        break;
+      }
       int wi_money;
-      std::cout << "Deposit Money\n";
+      std::cout << "Withdraw Money\n";
       std::cout << "Enter the amount of money you want to withdraw. : ";
       std::cin >> wi_money;
       withdraw(wi_money);
       std::cout << "It's Done!\n";
       break;
-    case '4':
+    case 4:
       char delchar;
       std::cout << "Delete Account\n";
       std::cout << "Are you sure? [Y/n]";
@@ -119,13 +131,16 @@ void account::displayLoop()
       }
       std::cout << "It's Done!\n";
       break;
+    case 5:
+      std::cout << "Logging Out...\n";
+      break;
     default:
       std::cout << "Please Enter Number from 1-5 only.\n";
       break;
     }
-  } while (ch != '5');
-  if (ch == '5')
+  } while (ch != 5);
+  if (ch == 5)
   {
-    std::cout << "Logging Out...\n";
+    login();
   }
 }
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Console Application
 Project detail:
   Date start:04/07/2020
   Basic ATM system
+05/07/2020
+  Add login page
+  Add create account
+  Change input menu to int
+  Add logout to lead to login page
+  Check If withdraw from 0
+  Check if account is not created
+  Clear username and password string
